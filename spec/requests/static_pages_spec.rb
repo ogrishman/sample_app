e9e4f57760b8ea858cb092a1f10ebed5543e336c
Ogrish Man <ogrishman@gmail.com>
2014-07-10 17:46:56 +0800

    Change routes and refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 describe "Home Pages" do
+
+	before	{visit home_path}
 	it "shoudl have content 'Sample App'" do
-	    visit 'static_pages/home'
 	    expect(page).to have_selector "h1", :text => "Sample App"
 	end
 
 	it "Should have the right title" do
-		visit 'static_pages/home'
 	    expect(page.title).to eq "Edde's Sample App | Home"
 	end
 
@@ -15,37 +15,34 @@ end
 
 
 describe "Help Pages" do
+	before {visit help_path}
 	it "shoudl have content 'Help'" do
-	    visit 'static_pages/help'
 	    expect(page).to have_selector "h1", :text => "Help"
 	   
 	end
 
 	it "Should have the right title" do
-		visit 'static_pages/help'
 		expect(page.title).to eq "Edde's Sample App | Help"
 	end
 end
 
 
 describe "About Pages" do
-	it "shoudl have content 'About US'" do
-	    visit 'static_pages/about'
 
+	before {visit about_path}
+	it "shoudl have content 'About US'" do
 	    expect(page).to have_selector "h1", :text => "About US"
 	end
 
 	it "Should have the right title" do
-		visit 'static_pages/about'
 		expect(page.title).to eq "Edde's Sample App | About US"
 	end
 end
 
 
 describe "Contact" do
+	before {visit contact_path}
     it "should have h1 contact" do
-        visit 'static_pages/contact'
-
         expect(page).to have_selector "h1", text: "Contact US"
     end
 end
